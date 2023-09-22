fa20906e594c86a46ebb119bee988f0aaa5eb3e8
CoRExE <98487589+CoRExE@users.noreply.github.com>
2023-09-23 00:08:32 +0200

    Init Poker Extension

---
+++ BotExtensions/Poker.py
@@ -0,0 +1,4 @@
+#  Copyright (c) 2023.
+#  Ceci est une propriété de CoRE.ExE, vous êtes autorisés à l'intégration de ce produit.
+#  Il est formellement interdit de monétiser ce contenu.
+#  Toute infraction aux règles précédemment citée pourra engager des poursuites.
