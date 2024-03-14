15528ffcdca7ae94406e883835d10f98c8829065
CoRE.ExE <98487589+CoRExE@users.noreply.github.com>
2024-03-14 15:17:12 +0100

    AI func:launch

--- BotExtensions/AIExtension.py
+++ BotExtensions/AIExtension.py
@@ -3,19 +3,29 @@
 #  Il est formellement interdit de monétiser ce contenu.
 #  Toute infraction aux règles précédemment citée pourra engager des poursuites.
 
-import discord
 from discord.ext import commands
 import requests
 import json
+import subprocess as sub
 
 
 def setup(bot):
     bot.add_cog(AIExtension(bot))
 
 
+def teardown(bot):
+    bot.remove_cog("AIExtension")
+
+
+def launch_ai():
+    sub.Popen(["ollama serve"])
+
+
 class AIExtension(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.serv_online = True if requests.get("http://localhost:11434").status_code == 200 else False
+        self.models = []
 
     @commands.slash_command()
     @commands.has_any_role("Admin")
